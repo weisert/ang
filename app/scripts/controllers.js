5b24e4a62c8399739d27f536c34de531bd81aa5c
Igor Vayzert <igor.weisert@gmail.com>
2016-02-04 22:29:54 +0600

    Exercise: Client-Server Communication using $http.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,7 +8,13 @@ angular.module('confusionApp')
         $scope.filtText = '';
         $scope.showDetails = false;
 
-        $scope.dishes = menuFactory.getDishes();
+        $scope.dishes= [];
+        menuFactory.getDishes()
+            .then(
+                function(response) {
+                    $scope.dishes = response.data;
+                }
+            );
 
         $scope.select = function (setTab) {
             $scope.tab = setTab;
@@ -83,8 +89,14 @@ angular.module('confusionApp')
     }])
 
     .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function ($scope, $stateParams, menuFactory) {
-        var dish = menuFactory.getDish(parseInt($stateParams.id, 10));
-        $scope.dish = dish;
+        $scope.dish = {};
+        menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish=true;
+                }
+            );
     }])
 
     .controller('DishCommentController', ['$scope', function ($scope) {
@@ -105,7 +117,14 @@ angular.module('confusionApp')
     }])
 
     .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function ($scope, menuFactory, corporateFactory) {
-        $scope.dish = menuFactory.getDish(0);
+        $scope.dish = {};
+        menuFactory.getDish(0)
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish = true;
+                }
+            );
         $scope.promotion = menuFactory.getPromotion(0);
         $scope.leader = corporateFactory.getLeader(0);
     }])
